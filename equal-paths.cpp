3332ef04ad8341cf413cac9d41cf25b5e038f101
Andrew Park <apark025@usc.edu>
2024-03-26 04:02:38 +0000

    Done with TreeTraversals

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,18 +1,42 @@
 #ifndef RECCHECK
-//if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <iostream>
 #endif
 
 #include "equal-paths.h"
 using namespace std;
 
-
+int checkEqualHelper(Node* node, int depth, int& targetDepth);
 // You may add any prototypes of helper functions here
 
 
-bool equalPaths(Node * root)
+bool equalPaths(Node* root)
 {
     // Add your code below
+    if(!root){
+      return true;
+    }
+    
 
+    int targetDepth = -1;
+    return checkEqualHelper(root,0,targetDepth);
 }
 
+int checkEqualHelper(Node* node, int depth, int& targetDepth){
+  if(!node){
+    return true;
+  }
+
+  if(!node->left && !node->right){
+    if(targetDepth == -1){
+      targetDepth = depth;
+    }
+    else{
+      if(depth != targetDepth){
+        return false;
+      }
+    }
+  }
+
+  return checkEqualHelper(node->left,depth + 1, targetDepth) &&
+          checkEqualHelper(node->right, depth + 1, targetDepth);
+}
